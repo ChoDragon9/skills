d4ee1c5730667be9f103e9ec540e395c8b877ade
junil-hwang <tjsdlf4261@naver.com>
2019-04-28 15:23:52 +0900

    #4 Board - Basic Phase 04

---
+++ 튜토리얼/Server-Side/1_Board_04/views/board/update.php
@@ -0,0 +1,35 @@
+<?php
+  // 기본 DB 정보 가져오기
+  $sql = "SELECT * FROM board WHERE idx = '{$_GET['idx']}'";
+  $row = $db->query($sql)->fetch(PDO::FETCH_OBJ);
+?>
+<form action="" method="post">
+  <fieldset>
+    <legend>글작성</legend>
+    <input type="hidden" name="action" value="update">
+    <ul>
+      <li>
+        <label>
+          제목
+          <input type="text" name="subject" value="<?php echo $row->subject?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          작성자
+          <input type="text" name="writer" value="<?php echo $row->writer?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          내용
+          <input type="text" name="content" value="<?php echo $row->content?>" size="80">
+        </label>
+      </li>
+      <li>
+        <button type="submit">완료</button>
+        <button type="button" onclick="location.href = './?page=view&amp;idx=<?php echo $row->idx?>'">취소</button>
+      </li>
+    </ul>
+  </fieldset>
+</form>
